318e2d03043887870f10786b304c5206d85dc30a
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2020-11-28 19:59:42 +0600

    Creating the newTodoForm component

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,14 +1,13 @@
-import React from "react";
-import TodoListItem from "./TodoListItem";
+import React from 'react';
+import NewTodoForm from './NewTodoForm';
+import TodoListItem from './TodoListItem';
+import './TodoList.css';
 
-const TodoList = ({ todos }) => {
-  return (
+const TodoList = ({ todos = [] }) => (
     <div className="list-wrapper">
-      {todos.map((todo) => (
-        <TodoListItem todo={todo} />
-      ))}
+        <NewTodoForm />
+        {todos.map(todo => <TodoListItem todo={todo} />)}
     </div>
-  );
-};
+);
 
 export default TodoList;
